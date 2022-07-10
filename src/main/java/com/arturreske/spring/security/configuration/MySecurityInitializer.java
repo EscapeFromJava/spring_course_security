b5b55f8f98fe73e55ad3df1955a01e50fc7b3b7e
EscapeFromJava <reske.artur@gmail.com>
2022-07-10 22:13:36 +0300

    v 1.9

---
+++ src/main/java/com/arturreske/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.arturreske.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
